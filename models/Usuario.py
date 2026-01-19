4aae29a98772ab08e4f0c85868eb05e0ae5e65a6
iruuu <irenemjh13@gmail.com>
2026-01-19 10:25:24 +0100

    Roles Usuarios

--- models/Usuario.py
+++ models/Usuario.py
@@ -5,9 +5,18 @@ class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
+    role = db.Column(db.String(20), default='user', nullable=False)
     
     # Relationships
     pedidos = db.relationship('Pedido', backref='usuario', lazy=True)
 
+    @property
+    def is_admin(self):
+        return self.role == 'admin'
+
+    @property
+    def is_user(self):
+        return self.role == 'user'
+
     def __repr__(self):
-        return f'<Usuario {self.username}>'
+        return f'<Usuario {self.username} ({self.role})>'

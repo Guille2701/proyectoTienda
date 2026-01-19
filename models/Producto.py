eab52a5a2fe25741add68883c0b6e6077ea1f0cb
iruuu <irenemjh13@gmail.com>
2026-01-19 10:01:47 +0100

    Imagenes Products

--- models/Producto.py
+++ models/Producto.py
@@ -6,6 +6,7 @@ class Producto(db.Model):
     nombre = db.Column(db.String(100), nullable=False)
     precio = db.Column(db.Float, nullable=False)
     stock = db.Column(db.Integer, nullable=False)
+    imagen_url = db.Column(db.String(255), nullable=True)
     
     # Relationships
     detalles = db.relationship('Detalle_Pedido', backref='producto', lazy=True)
